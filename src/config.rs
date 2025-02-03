d76fa09bd308b4b7df3a7d12ff3b111f562c42a8
xsw2009 <1668890909@qq.com>
2025-02-03 20:44:04 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -97,7 +97,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["xsw2009.dynv6.net"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["xsw.8890909.xyz"];
 pub const RS_PUB_KEY: &str = "W9D16OBq4bueee7TLhK+1r0SYqBynAaxr8uFE0M6L9M=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
