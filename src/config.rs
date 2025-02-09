748bbba70f8f7f31d75a6d941420f0143933c423
IMStepFather <lx6688@88.com>
2025-02-10 07:19:44 +0800

    修改101行API_SERVER地址

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["hwrustdesk.lxjl.love"];
+pub const API_SERVER: &str = "https://hwrustdesk.lxjl.love";
 pub const RS_PUB_KEY: &str = "Lixi40233251";
-pub const API_SERVER: &str = "https://Lixi40233251";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
