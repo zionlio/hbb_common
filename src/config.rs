ceb1c0c180154e21277340d8fb02d74d4a80251b
lidengwen <57475914+lidengwen@users.noreply.github.com>
2025-02-20 16:47:45 +0800

    Update config.rs
    ldw

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["liantek.cn"];
+pub const RS_PUB_KEY: &str = "a14x5ghZkL7mCtNW1FFfLii4Rw88vjB8HNaTbO1IlQ0=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
