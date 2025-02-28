6b752703a1bc70f69a24d6f08fcc19c5275cb11d
rmfgh <85973064+rmfgh@users.noreply.github.com>
2025-03-01 02:35:15 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.rmfit.cn"];
+pub const RS_PUB_KEY: &str = "gqTUXHoZb812W4dpRehYuVspbke1VQiegbB7NK22RcQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
