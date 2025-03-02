f30ee1990ad00cff11174a2a385a03cf3f2ca94f
bdh2022 <117006615+bdh2022@users.noreply.github.com>
2025-03-02 10:01:19 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["39.172.42.14"];
+pub const RS_PUB_KEY: &str = "KJzNYE6AGnZOBBDIiAYE3B1zfroVX8oh3v5sDSLRHvs=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
