7f8184b1fde82d546a57a7aa379b5da0f2453944
wlhl1h <57785374+wlhl1h@users.noreply.github.com>
2025-03-06 13:01:46 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.3.240"];
+pub const RS_PUB_KEY: &str = "PUEOpRKV04upfdtxKUAV1wd5eqzgVrrzn8Zc0KD4BKg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
