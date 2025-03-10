042f308deb2269b2be06609de2337ac7a7a70f54
xunguo124 <88746490+xunguo124@users.noreply.github.com>
2025-03-10 19:30:08 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nas.xsgyun.top"];
+pub const RS_PUB_KEY: &str = "Bf1iCHRrM3NlynquVLPtx3HHnHv7YexF+3wFMQrcTSk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
