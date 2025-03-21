716c110c93d97a181852e195404ff6ca81136017
paulwg128 <wgpaul128@gmail.com>
2025-03-21 13:36:05 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["vt03-sg.duckdns.org"];
+pub const RS_PUB_KEY: &str = "Sezy7EKQZI7NbvtkIRiRDfgm50pa5Us9t7eXJbPw3U4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
