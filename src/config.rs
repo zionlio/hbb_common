fbea5234e2d6261094efe44a203d02fc8f1db116
Buns <78155743+hxs664467671@users.noreply.github.com>
2025-03-21 11:31:44 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.331761.xyz"];
+pub const RS_PUB_KEY: &str = "5r9PD0GB1OIslQklSOO1o4oxgvfzk5tExyWB2ESrCH4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
