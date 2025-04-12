9e3d2d0d3f1e9d435f77ccb987fb5d4889012ea6
Dong-ac86u <65331696+Dong-ac86u@users.noreply.github.com>
2025-04-12 11:50:26 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,10 +98,10 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["lxiaodong.eu.org"];
+pub const RS_PUB_KEY: &str = "L1arYzAuO3BFwT4L9zrnoQtSL+Rwxxtir4zOFbVyzP4=";
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
+pub const RENDEZVOUS_PORT: i32 = 21116; 
 pub const RELAY_PORT: i32 = 21117;
 
 macro_rules! serde_field_string {
