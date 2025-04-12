10262a2ba43c6279b7a788e68893edbb928a3d8c
sjhqq <sjhqqq@qq.com>
2025-04-12 16:54:02 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.sjhqqq.top"];
+pub const RS_PUB_KEY: &str = "+aTMn5izA3vcdMSgUOc7e4wA8qah9yshexv9eS9UIDY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
