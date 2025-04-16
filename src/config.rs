90d205fa39b014fb75a8e81f3b612c641cdfa270
Yunpeng333 <58073204+Yunpeng333@users.noreply.github.com>
2025-04-16 19:50:21 +0800

    rustdesk/hbb_common

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["218.27.16.117"];
+pub const RS_PUB_KEY: &str = "sMeEk9pC08zleTLCQDDAIm7TCEehuDXisFETFAWSuYM=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
