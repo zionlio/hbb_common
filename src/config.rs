3a2c7c25fd0d93fa2e78479552629fd5ffa55678
Tao Guang <74671375+1158523499@users.noreply.github.com>
2025-04-17 16:39:58 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.238.134.143"];
+pub const RS_PUB_KEY: &str = "5zsY7mIhKDpoJMW9zOqTJHAr20nbfhiTjAQgIiUmkdE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
