2c990997e2ee0a5bc8ec6f01a8fec63a271d18b5
TDX2020 <71589685+TDX2020@users.noreply.github.com>
2025-04-22 01:36:02 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["fastnas.online"];
+pub const RS_PUB_KEY: &str = "RsE1Fc+7u3oUHVuQK7E1FGo60aruJBoROtdvcHKaS7U=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
