aeec8d2e0a4014f8b0d29f7ef466fc40c3e2f723
leeying123 <1039763530@qq.com>
2025-04-22 14:54:36 +0800

    更换服务器

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["http://175.6.7.147:21114"];
+pub const RS_PUB_KEY: &str = "dOrLgYN+yntlBYKywj0QhJHxl3RiqD+hLP2vY+ZIVyA=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
