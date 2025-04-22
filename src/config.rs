56f6485d6d5caf02cea141ae972fe60238e3f29e
superstargoto <137243100+superstargoto@users.noreply.github.com>
2025-04-22 19:07:56 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["1236699.xyz"];
+pub const RS_PUB_KEY: &str = "2ID7yAbFcpOqDgQadRe9wepR4Qv3BCB516HR+Ay3L2A=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
