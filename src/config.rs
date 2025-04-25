f2d9a1913aad06766300bd0e7bfab006e542eff3
xhf <panluning233@163.com>
2025-04-25 23:16:56 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["110.42.45.185"];
+pub const RS_PUB_KEY: &str = "IbDhQOl4Sua56YZpij8F7wpvlxN0FKomqQYe0YE9kY0=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
