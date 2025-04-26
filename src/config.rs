b262da9c84cfbe28db2fff96f61f8ca179355a3e
wswswawa <shaoxiaopai@foxmail.com>
2025-04-26 21:30:04 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["op.shaoxiaorustdesk.cn"];
+pub const RS_PUB_KEY: &str = "jLm118RrwuflaT3kDhAQJoGl2lOZNbfPjyASIb6BJ7o=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
