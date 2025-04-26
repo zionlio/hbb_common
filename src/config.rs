4e63cdfb55379b13ef3612728b4cf965818a76f2
muxuejin <muxuejin@outlook.com>
2025-04-26 21:44:53 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.108.193.171"];
+pub const RS_PUB_KEY: &str = "K5a5+fzRLiC5ZXhIwHpbY6w+VZCWmoC1y3vq7DlBdqk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
