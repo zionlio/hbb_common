b440cc8a2257008f900c2e7c58f070b00b29888c
yxrswjwl <46105937+yxrswjwl@users.noreply.github.com>
2025-04-27 14:40:09 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["xiongtieshu.top"];
+pub const RS_PUB_KEY: &str = "0xuQuKZ3XKd02Rlx5RUntQhwhovbiAchq2kFuahLXkE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
