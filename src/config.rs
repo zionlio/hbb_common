b116f3b848e72269b201f185c720f38832d4645c
ChenXing <xusc@yicnt.com>
2025-04-30 18:16:38 +0800

    改为自己的中继服务器

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["121.229.16.163"];
+pub const RS_PUB_KEY: &str = "bla4G7zDX9J2NN7kVykfiAZIPcfcnTGG6iKmfVsXw+Y=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
