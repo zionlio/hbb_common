199c839283cecf893d60c6d27d6170d5218310f4
terrydyinfo <terry@dyinfo.com.tw>
2025-05-06 16:20:39 +0800

    ok

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.dyinfo.com.tw"];
+pub const RS_PUB_KEY: &str = "56DS1I98bASod4TWeAwgUEnC1p01QRvuNHk0LfHecBY";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
