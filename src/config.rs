583379683e75e2c7cea7b053369d3fce6cc84893
coldcen <aiyou5201314@outlook.com>
2025-05-13 10:19:18 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.zytintra.com"];
+pub const RS_PUB_KEY: &str = "QGmqsHPgclSSbx5j5OmWzd8ACXLAWUFtkvEi5XdDmZo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
