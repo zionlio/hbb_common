d379834c6affc90c23f879475b974034d45e8c9c
veenyi <60565189+veenyi@users.noreply.github.com>
2025-05-14 22:48:54 +0800

    Update config.rs
    update server

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["http://183.236.245.250"];
+pub const RS_PUB_KEY: &str = "2JljKWZcu9sDPZnm3cyOk4etIweMira8VbJGKgprTj0";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
