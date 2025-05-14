2283e3fba0bf9723197998743dd5f26d82a69c27
aychyj260 <57698569+aychyj260@users.noreply.github.com>
2025-05-14 23:25:27 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["81.70.160.27"];
+pub const RS_PUB_KEY: &str = "pZ+qfhSWVG99KHSV9QW4WOTP3OatRcSAzqmdPFuiw2c=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
