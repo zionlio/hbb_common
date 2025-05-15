ddfd0f1a5e725b7ca803a3463a7bb73c978ac899
kkquincy <kkquincy@icloud.com>
2025-05-15 21:19:40 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.kkquincy.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "PVAXCBDrim4en+U96ZtHXuhkEQ75UvLjhyvtCvOaOr4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
