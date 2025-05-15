f588054897e14b9197899b9b30e2f64e8aa6a552
boy0916 <moroke@163.com>
2025-05-15 20:36:37 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["api.ebai.cn"];
+pub const RS_PUB_KEY: &str = "sjyvIOq1bK6k07Ho+p6ibH0xuCAI0usRJdqUMs3SG8Q=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
