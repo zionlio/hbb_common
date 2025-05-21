f7dfac3c1449a062ed19c712c0f1329ef676973e
MrQQY <166140876+MrQQY@users.noreply.github.com>
2025-05-21 18:58:38 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["39.104.51.2"];
+pub const RS_PUB_KEY: &str = "cp0SWPRRmvxM0Mtsvx+I04XJHm3juZ3XiEJCwtDd0Uk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
