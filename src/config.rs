4d500337dcb41b661fec067fe430e3d16f8a4672
wilson-zhang2025 <ghty332200@qq.com>
2025-06-07 12:56:12 +0800

    update config

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,9 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.ponieintl.com"];
+pub const RS_PUB_KEY: &str = "oqF5OtBVg+KqCHA97v6ZzNPVG6VP7K5cmKbcTPhaAkY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
