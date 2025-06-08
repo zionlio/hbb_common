e4b7b34c087c1eb8d4929ba4c550278c61affc78
as12356sa <109841644+as12356sa@users.noreply.github.com>
2025-06-08 14:56:28 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["14.103.175.42"];
+pub const RS_PUB_KEY: &str = "akmO15Xq5fGRUsxnOMCy1gTc+l+8lUbnRJ7t8YVIF4E=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
