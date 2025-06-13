c730919f425890c865317f137c02f52a6059a981
ait-lqm <lqm@mail.alpha-it-system.com>
2025-06-13 14:53:37 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.a-it.jp"];
+pub const RS_PUB_KEY: &str = "PqhYFEfvPNdUaKZCGLf+2GpsRU8w2o0HAe3EXkb5rz4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
