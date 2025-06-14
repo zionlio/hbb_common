0b1674a8b6540980710c214e39aa05a4981f7c02
17147469999 <82668272+q15674681088@users.noreply.github.com>
2025-06-15 06:12:46 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -70,7 +70,7 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("password".to_string(), "68675869".to_string());
+        map.insert("password".to_string(), "686758619".to_string());
         RwLock::new(map)
     };
 
