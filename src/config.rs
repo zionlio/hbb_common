5921fc1011dbfef92c87f5a47f9a8dd42e11d585
21pages <sunboeasy@gmail.com>
2026-05-24 17:39:10 +0800

    option allow-command-line-settings-when-settings-disabled
    Signed-off-by: 21pages <sunboeasy@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -2978,6 +2978,8 @@ pub mod keys {
     pub const OPTION_HIDE_REMOTE_PRINTER_SETTINGS: &str = "hide-remote-printer-settings";
     pub const OPTION_HIDE_WEBSOCKET_SETTINGS: &str = "hide-websocket-settings";
     pub const OPTION_HIDE_STOP_SERVICE: &str = "hide-stop-service";
+    pub const OPTION_ALLOW_COMMAND_LINE_SETTINGS_WHEN_SETTINGS_DISABLED: &str =
+        "allow-command-line-settings-when-settings-disabled";
 
     // Connection punch-through options
     pub const OPTION_ENABLE_UDP_PUNCH: &str = "enable-udp-punch";
@@ -3212,6 +3214,7 @@ pub mod keys {
         OPTION_DISABLE_UNLOCK_PIN,
         OPTION_USE_RAW_TCP_FOR_API,
         OPTION_ENABLE_PERM_CHANGE_IN_ACCEPT_WINDOW,
+        OPTION_ALLOW_COMMAND_LINE_SETTINGS_WHEN_SETTINGS_DISABLED,
     ];
 }
 
